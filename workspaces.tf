7e0b4a3610ea4134eee84d8ea3f56f82148946d4
Carl Javier <carl.javier@hashicorp.com>
2021-11-03 13:53:01 +1100

    added test and prod workspaces

--- workspaces.tf
+++ workspaces.tf
@@ -3,3 +3,35 @@ resource "tfe_workspace" "app" {
   organization = tfe_organization.org["carlcorp"].name
   tag_names    = ["test", "app"]
 }
+
+
+resource "tfe_workspace" "app-dev" {
+
+    for_each = var.workspace-app
+
+    name = format("%s-dev",each.value.name)
+    organization = tfe_organization.org["carlcorp"].name
+    tag_names = concat(["dev"],each.value.tag_names)
+  
+}
+
+resource "tfe_workspace" "app-test" {
+
+    for_each = var.workspace-app
+
+    name = format("%s-test",each.value.name)
+    organization = tfe_organization.org["carlcorp"].name
+    tag_names = concat(["test"],each.value.tag_names)
+  
+}
+
+
+resource "tfe_workspace" "app-prod" {
+
+    for_each = var.workspace-app
+
+    name = format("%s-prod",each.value.name)
+    organization = tfe_organization.org["carlcorp"].name
+    tag_names = concat(["prod"],each.value.tag_names)
+  
+}
